807922dbb31eeacc1c00bd65152eefd221956e49
Hani Jardack <hani.jardack@proton.me>
2025-12-06 09:08:09 -0500

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,48 @@
+import random
+
+
+#Basic Password Generator
+
+def main():
+    
+    password = ''
+    
+    sample_space_standard = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYS0123456789'
+    
+    sample_space_special_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYS0123456789!@#$%^&*'
+    
+    number_of_chars = input("Enter desired number of characters. ")
+    
+    default_number_of_chars = 14
+    
+    if number_of_chars == '':
+        number_of_chars = default_number_of_chars
+    
+    include_special_chars = input("Include special characters? Y/n ")
+    
+    if include_special_chars in ('Y', 'y' 'Yes', 'yes', ''):
+        while len(password) < int(number_of_chars):
+            password += random.choice(sample_space_special_chars)
+    elif include_special_chars in ('n', 'N' 'No', 'NO'):
+        while len(password) < int(number_of_chars):
+            password += random.choice(sample_space_standard)
+    else:
+        while len(password) < int(number_of_chars):
+            password += random.choice(sample_space_special_chars)
+        
+            
+    print(password)
+    
+main()
+        
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
